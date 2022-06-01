76a670e6482725c884d1c3c63be3138ad3667bad
chaitali Trivedi <chaitupt@gmail.com>
2022-06-01 17:32:49 +0530

    0.5

--- MobileApp/MobileApp/Controllers/OrderProductController.cs
+++ MobileApp/MobileApp/Controllers/OrderProductController.cs
@@ -24,12 +24,66 @@ namespace MobileApp.Controllers
                     db.OrderProducts.Add(OrderProduct1);
                     db.SaveChanges();
                 }
+                OrderProduct1.UpdatedDate = DateTime.Now;
+                db.Entry(OrderProduct1).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK, OrderProduct1);
+
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
             }
-
+        }
+        [HttpGet]
+        public HttpResponseMessage DeleteOrderProduct(Guid orderProductId)
+        {
+            try
+            {
+                MobileStoreEntities1 db = new MobileStoreEntities1();
+                var _Orderproduct = db.Orders.FirstOrDefault(x => x.Id == orderProductId);
+                if (_Orderproduct == null)
+                {
+                    throw new Exception("There Is No Order Product Like This");
+                }
+                _Orderproduct.IsDeleted = true;
+                _Orderproduct.CreatedDate = DateTime.Now;
+                _Orderproduct.UpdatedDate = DateTime.Now;
+                db.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpGet]
+        public HttpResponseMessage GetOrderProductById(Guid orderProductId)
+        {
+            try
+            {
+                MobileStoreEntities1 db = new MobileStoreEntities1();
+                var _OrderProduct = db.Addresses.FirstOrDefault(x => x.Id == orderProductId && x.IsDated != true);
+                return Request.CreateResponse(HttpStatusCode.OK, _OrderProduct);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        [HttpGet]
+        public HttpResponseMessage GetOrderProductByUserId(Guid userId)
+        {
+            try
+            {
+                MobileStoreEntities1 db = new MobileStoreEntities1();
+                var _Order = db.Orders.FirstOrDefault(x => x.userId == userId);
+                return Request.CreateResponse(HttpStatusCode.OK, _Order);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
         }
     }
 }
