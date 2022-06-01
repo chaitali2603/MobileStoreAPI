d4cea8b8eb9d2bd60148da0610e601d34e3a9dbb
chaitali Trivedi <chaitupt@gmail.com>
2022-06-01 13:43:11 +0530

    0.4

---
+++ MobileApp/MobileApp/Controllers/OrderProductController.cs
@@ -0,0 +1,35 @@
+﻿using MobileApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MobileApp.Controllers
+{
+    public class OrderProductController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage CreateOrderProduct(OrderProduct OrderProduct1)
+        {
+            try
+            {
+                MobileStoreEntities1 db = new MobileStoreEntities1();
+                if (OrderProduct1.Id == Guid.Empty)
+                {
+                    OrderProduct1.Id = Guid.NewGuid();
+                    OrderProduct1.CreatedDate = DateTime.Now;
+                    OrderProduct1.UpdatedDate = DateTime.Now;
+                    db.OrderProducts.Add(OrderProduct1);
+                    db.SaveChanges();
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
+        }
+    }
+}
