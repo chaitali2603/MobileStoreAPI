021b6d090c851c42114afe56d6ba5ab54f935dbb
chaitali Trivedi <chaitupt@gmail.com>
2022-06-12 13:52:53 +0530

    12-06-2022

---
+++ MobileApp/MobileApp/Dtos/LogInResponceDto.cs
@@ -0,0 +1,15 @@
+﻿using MobileApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileApp.Dtos
+{
+    public class LogInResponceDto
+    {
+        public AppUser User { get; set; }
+        public string Token { get; set; }
+
+    }
+}
